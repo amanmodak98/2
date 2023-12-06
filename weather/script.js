a8bd483fe9b49ca46ddb9ff5a78f92280d9ac401
Aman Kumar Modak <amanmodak98@gmail.com>
2023-12-07 02:55:31 +0530

    Update script.js

--- weather/script.js
+++ weather/script.js
@@ -1,7 +1,7 @@
 function getWeather() {
     const cityInput = document.getElementById("cityInput").value;
     const apiKey = "2ef13c52ff58b2157ed17d8670429677";
-    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&APPID=2ef13c52ff58b2157ed17d8670429677&units=metric';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&APPID=${apiKey}&units=metric`;
 
     fetch(apiUrl)
         .then(response => response.json())
@@ -9,13 +9,22 @@ function getWeather() {
             if (data.cod === "404") {
                 document.getElementById("weatherInfo").innerHTML = "City not found";
             } else {
-                const weatherInfo = `
-                    <p>City: ${data.name}, ${data.sys.country}</p>
-                    <p>Temperature: ${data.main.temp}°C</p>
-                    <p>Weather: ${data.weather[0].description}</p>
-                `;
-                document.getElementById("weatherInfo").innerHTML = weatherInfo;
+                // Use the actual fetched data
+                processWeatherData(data);
             }
         })
         .catch(error => console.error("Error fetching weather data:", error));
+}
+
+// Define a callback function to process the weather data
+function processWeatherData(data) {
+    console.log("Processing weather data:");
+    console.log("Location:", data.name);
+    console.log("Temperature:", data.main.temp);
+    console.log("Description:", data.weather[0].description);
+    // Add more processing logic as needed
+
+    // Example: Update an HTML element with the weather information
+    const weatherInfoElement = document.getElementById("weatherInfo");
+    weatherInfoElement.innerHTML = `Location: ${data.name}<br>Temperature: ${data.main.temp}°C<br>Description: ${data.weather[0].description}`;
 }
